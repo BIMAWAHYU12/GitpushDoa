0a79a05d01704f175c7c691b751e1b6fb4565a0c
Farhan Apriliansyah <muhamadfarhanapriliansyah29@gmail.com>
2026-03-13 00:35:17 +0700

    feat: Farhan menyelesaikan API Dashboard & Monitoring

---
+++ backend/controllers/dashboardController.js
@@ -0,0 +1,18 @@
+const db = require("../config/db");
+
+exports.getDashboard = (req, res) => {
+    const query = `
+        SELECT
+        (SELECT COUNT(*) FROM barang) AS total_barang,
+        (SELECT COUNT(*) FROM kategori) AS total_kategori,
+        (SELECT COUNT(*) FROM transaksi_stok) AS total_transaksi
+    `;
+
+    db.query(query, (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.json(result);
+        }
+    });
+};

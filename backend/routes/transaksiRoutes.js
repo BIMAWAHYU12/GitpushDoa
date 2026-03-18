14358966dd810126b66e5d6390d813b6d2416149
Yurida26 <yahsyayurida@gmail.com>
2026-03-18 17:52:13 +0700

    feat: Yurida  Menyelesaikan SPRINT 3: Update API Riwayat Transaksi dengan Multi-Table JOIN

--- backend/routes/transaksiRoutes.js
+++ backend/routes/transaksiRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const transaksiController = require('../controllers/transaksiControllers');
+const transaksiController = require('../controllers/transaksiController');
 
-// Endpoint untuk mendapatkan semua riwayat
-router.get('/riwayat', transaksiController.getRiwayatTransaksi);
-
-// Endpoint untuk filter berdasarkan tipe (IN/OUT)
-router.get('/laporan/:tipe', transaksiController.getLaporanByTipe);
+// Endpoint utama untuk tabel riwayat di Frontend
+router.get('/riwayat-lengkap', transaksiController.getRiwayatLengkap);
 
 module.exports = router;
